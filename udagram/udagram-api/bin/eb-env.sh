3cbe51ec0a85b2ca1d6e9f7e89c1895bf2577b27
Mohamed Hamdy Mahfouz <83695141+muhamadhamdy@users.noreply.github.com>
2022-12-28 14:43:19 +0200

    Update eb-env.sh

--- udagram/udagram-api/bin/eb-env.sh
+++ udagram/udagram-api/bin/eb-env.sh
@@ -1,17 +1,13 @@
-eb open udagram-m-hamdy-dev 
-eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgre2022
-eb setenv POSTGRES_DB=ebdb
-eb setenv POSTGRES_HOST=udagram-m-hamdy-rds.c9ivzb6vpn4o.us-east-1.rds.amazonaws.com
-eb setenv AWS_REGION=us-east-1
-eb setenv AWS_PROFILE=default
-eb setenv DB_PORT=5432
-eb setenv APP_PORT=8090
-eb setenv AWS_BUCKET=udagram-m-hamdy-s3-001
-eb setenv URL=http://udagram-m-hamdy-dns.us-east-1.elasticbeanstalk.com:8090/
-eb setenv JWT_SECRET=bdh3j5f678g9v06fc42h1g1g5f8n9j90df1fd9bfd
-eb setenv AWS_ACCESS_KEY_ID=AKIAUDT7GXFKUA4RJZEN
-eb setenv AWS_SECRET_ACCESS_KEY=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
-
-
-eb open udagram-m-hamdy-dev 
-eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgre2022 POSTGRES_DB=ebdb POSTGRES_HOST=udagram-m-hamdy-rds.c9ivzb6vpn4o.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1 AWS_PROFILE=default DB_PORT=5432 APP_PORT=8090 AWS_BUCKET=udagram-m-hamdy-s3-001 URL=http://udagram-m-hamdy-dns.us-east-1.elasticbeanstalk.com:8090/ JWT_SECRET=bdh3j5f678g9v06fc42h1g1g5f8n9j90df1fd9bfd AWS_ACCESS_KEY_ID=AKIAUDT7GXFKUA4RJZEN AWS_SECRET_ACCESS_KEY=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
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME 
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv AWS_REGION=$AWS_REGION
+eb setenv AWS_PROFILE=$AWS_PROFILE
+eb setenv DB_PORT=$DB_PORT
+eb setenv APP_PORT=$APP_PORT
+eb setenv AWS_BUCKET=$AWS_BUCKET
+eb setenv URL=$URL
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
